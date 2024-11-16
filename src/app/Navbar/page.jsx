dd167cf472d1cbff54f17c889aa7742bd25757bb
wrixcode <wrixcode@gmail.com>
2024-11-16 15:44:56 +0530

    ss

--- src/app/Navbar/page.jsx
+++ src/app/Navbar/page.jsx
@@ -15,7 +15,7 @@ function Navbar() {
 
   return (
     <nav className="bg-[#101125]  fixed top-0 text-white shadow-md w-full">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className=" px-2 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className=" ">
