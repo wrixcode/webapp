98f03c0204e80d2c89d6470470965719231b2523
wrixcode <wrixcode@gmail.com>
2024-12-11 11:32:36 +0530

    hss

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import Services from './Services/page'
 
 import Contact from './Contact/page'
 
+
 const page = () => {
   return (
     <div>
@@ -14,6 +15,7 @@ const page = () => {
       <About/>
       <Services/>
       <Contact/>
+      
     </div>
   )
 }
